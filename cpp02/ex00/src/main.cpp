6f4c39e0ea68fd8807c7ee98ec57f7f462fcc5cd
alexlimfl <132150229+alexlimfl@users.noreply.github.com>
2024-05-14 20:48:39 +0800

    starting cpp02

---
+++ cpp02/ex00/src/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: folim <folim@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/13 13:50:41 by folim             #+#    #+#             */
+/*   Updated: 2024/05/13 13:56:31 by folim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "../include/Fixed.hpp"
+
+int main( void ) {
+    Fixed a;
+    Fixed b( a );
+    Fixed c;
+    c = b;
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+    return 0;
+}
