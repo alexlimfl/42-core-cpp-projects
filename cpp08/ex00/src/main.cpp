67515a7852175941e416d7d78c862853b11a12fa
alexlimfl <alexlimfl23@gmail.com>
2024-09-07 23:08:55 +0800

    wsl test

--- cpp08/ex00/src/main.cpp
+++ cpp08/ex00/src/main.cpp
@@ -17,12 +17,22 @@ int main(){
     int arr[] = {1, 2, 3, 4, 5};
     std::vector<int> vec(arr, arr + sizeof(arr)/sizeof(arr[0]));
     std::list<int> list(arr, arr + sizeof(arr)/sizeof(arr[0]));
+    std::deque<int> deque(arr, arr + sizeof(arr)/sizeof(arr[0]));
 
+    std::cout << "\nTesting vector container:\n";
     easyfind(vec, -1);
     easyfind(vec, 0);
     easyfind(vec, 5);
-    easyfind(list, -11);
-    easyfind(list, 11);
+
+    std::cout << "\nTesting list container:\n";
+    easyfind(list, -1);
+    easyfind(list, 0);
     easyfind(list, 5);
+
+    std::cout << "\nTesting deque container:\n";
+    easyfind(deque, -1);
+    easyfind(deque, 0);
+    easyfind(deque, 5);
+
     return 0;
 }
